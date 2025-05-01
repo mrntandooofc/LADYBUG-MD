dea380cd2784eb96e17792efe506fa7cd7def87c
mrntandooofc <ntandoofc@gmail.com>
2025-05-02 01:13:14 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '𝙻𝙰𝙳𝚈𝙱𝚄𝙶-𝙼𝙳',
+  author: 'ᴩᴏᴡᴇʀᴇᴅ ʙy ᴍʀ ɴᴛᴀɴᴅᴏ ᴏꜰᴄ ᴀɴᴅ ᴛʜʀɪ_ɪꜱ_ᴋɪᴅ',
+  botName: "LADYBUG-MD",
+  botOwner: 'ᴍʀ ɴᴛᴀɴᴅᴏ ᴏꜰᴄ', // Your name
+  ownerNumber: '+263771629199', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.0",
+};
+
+module.exports = settings;
