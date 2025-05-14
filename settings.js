a6b942ecd65a118ead650cc573889fbf78088769
mrntandooofc <ntandoofc@gmail.com>
2025-05-14 21:12:14 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "𝙻𝙰𝙳𝚈𝙱𝚄𝙶-𝙼𝙳",
   botOwner: 'Mr Ntando Ofc', // Your name
-  ownerNumber: '263771629199', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '263777124998', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
